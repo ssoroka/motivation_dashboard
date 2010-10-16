b2df6b45b0b00b0e2784bae958d23a8bf478de59
Jonathan Rudenberg <jonathan@titanous.com>
2010-10-16 06:23:25 -0500

    Shopify monthly sales

--- lib/integrations/shopify.rb
+++ lib/integrations/shopify.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'shopify_api'
+require 'active_support/time'
 
 class ShopifyIntegration
 
@@ -24,10 +25,91 @@ class ShopifyIntegration
   end
 
   def unfullfilled_orders
-    unshipped = ShopifyAPI::Order.find(:all, :params=> {:fulfillment_status => 'unshipped', :fields => 'id'})
-    partial = ShopifyAPI::Order.find(:all, :params=> {:fulfillment_status => 'partial', :fields => 'id'})
-    unshipped.length + partial.length
+    unshipped = ShopifyAPI::Order.count(:fulfillment_status => 'unshipped')
+    partial = ShopifyAPI::Order.count(:fulfillment_status => 'partial')
+    unshipped + partial
+  end
+
+  def monthly_sales
+    month_to_date = {}
+    ShopifyAPI::Order.find_each(:params => { 
+        :created_at_min => Time.now.beginning_of_month,
+        :fields => 'created-at,subtotal-price' }) do |order|
+      day = order.created_at.day
+      month_to_date[day] ||= 0
+      month_to_date[day] += order.subtotal_price
+    end
+
+    last_month = {}
+    ShopifyAPI::Order.find_each(:params => {
+        :created_at_min => Time.now.beginning_of_month - 1.month,
+        :updated_at_max => Time.now.beginning_of_month - 1.day,
+        :fields => 'created-at,subtotal-price' }) do |order|
+      day = order.created_at.day
+      last_month[day] ||= 0
+      last_month[day] += order.subtotal_price
+    end
+
+    month_to_date = (0...Time.now.day).map do |day|
+      month_to_date[day+1] || 0
+    end
+
+    last_month = (0...(Time.now.beginning_of_month - 1.day).day).map do |day|
+      last_month[day+1] || 0
+    end
+
+    { :last_month => last_month, :month_to_date => month_to_date }
   end
 
 
+end
+
+
+# Batch helpers from http://github.com/jstorimer/shopify_app
+module ShopifyAPI
+ class Base < ActiveResource::Base
+    def self.find_each(options = {})
+      find_in_batches(options) do |records|
+        records.each { |record| yield record }
+      end
+
+      self
+    end
+
+    def self.find_in_batches(options = {})
+      options[:params] ||= {}
+      page = 1
+      limit = options.delete(:limit) || 100
+
+      objects = find(:all, :params => {:page => page, :limit => limit}.merge(options[:params]))
+
+      while objects.any?
+        yield objects
+
+        break if objects.size < limit
+        page += 1
+        objects = find(:all, :params => {:page => page, :limit => limit}.merge(options[:params]))
+      end
+    end
+
+    def self.count(options = {})
+      options[:params] ||= {}
+      count = 0
+
+      page = 1
+      limit = options.delete(:limit) || 100
+
+      objects = find(:all, :params => {:page => page, :limit => limit}.merge(options[:params]))
+      count += objects.size
+
+      while objects.any?
+        break if objects.size < limit
+        page += 1
+        objects = find(:all, :params => {:page => page, :limit => limit}.merge(options[:params]))
+        count += objects.size
+      end
+
+      count
+    end
+  end
 end
