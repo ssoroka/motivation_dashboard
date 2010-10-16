204e68d2efd8d98f0856a33883ce07aa01bfa52b
Andrew Szczepanski <andrewkszczepanski@gmail.com>
2010-10-16 12:26:06 -0500

    Google cal integration

---
+++ lib/integrations/google_cal.rb
@@ -0,0 +1,25 @@
+require 'gcal4ruby'
+
+class GoogleCalIntegration
+
+  def self.perform(*args)
+    new(*args).perform
+  end
+
+  def initialize(options)
+    @service = GCal4Ruby::Service.new
+    @service.authenticate(options[:gmail], options[:password])
+    @events = @service.events
+  end
+
+  def perform
+    events
+  end
+
+  def events
+    counts = @events.map do |event|
+      [event.title, event.start_time.strftime("%I:%M%p %m/%d/%Y"), event.end_time.strftime("%I:%M%p %m/%d/%Y"), event.attendees.size]
+    end
+  end
+  
+end
