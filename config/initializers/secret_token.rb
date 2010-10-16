78d47d2a7a0b4a5f0b231cf465464baea097cc6a
Nathan Bertram <nbertram@gmail.com>
2010-10-15 19:01:25 -0500

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotivationdashboardCom::Application.config.secret_token = '0e58afd0269df69f015a17910b9819ac1c9f60b7a4b05f5df0397eb1b8a067c6ea559a3d0e3d0ea896ca24a337b5c99d9692f4296d0c76811ec5d6b05759f7e1'
