70054795cfc385551947462828b2f245d6e8a85a
Jonathan Rudenberg <jonathan@titanous.com>
2010-10-16 15:15:33 -0500

    Fix typos

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,5 +1,5 @@
 class Integration
-  INTEGRATIONS = {:github => 1, :basecamp => 2, :get_statisfaction => 3, :gmail => 4, :google_analytics => 5, :shopify => 6}
+  INTEGRATIONS = {:github => 1, :basecamp => 2, :get_satisfaction => 3, :gmail => 4, :google_analytics => 5, :shopify => 6}
  
   def self.for_select
     Integration::INTEGRATIONS.collect{|a,b| [a.to_s.humanize,b]}
