6f20c1d1c786ec83596bc41e061a683b33556342
Nathan Bertram <nbertram@gmail.com>
2010-10-17 10:51:33 -0500

    Styled data sources page

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,6 +1,5 @@
 class Integration
-  INTEGRATIONS = {:github => 1, :basecamp => 2, :get_satisfaction => 3, :gmail => 4, :google_analytics => 5, :shopify => 6, :mail_chimp => 7}
-  VIEW_SHOW_WORDS = { :shopify => "Shopify", :mail_chimp => "Mail Chimp" }
-  VIEW_COLOUR = { :github => '#FFF', :get_satisfaction => "purple", :gmail => '#FFF', :google_analytics => '#FFF'}
+  # basecamp => 2 later
+  INTEGRATIONS = {:github => 1, :get_satisfaction => 3, :gmail => 4, :google_analytics => 5, :shopify => 6, :mail_chimp => 7}
 end
  
